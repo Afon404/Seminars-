66b07be441741bce9ed1d0a11dd655730c1d9151
Afon404 <>
2023-01-18 14:01:22 +0300

    Вывести квадрат числа.

--- Seminar_1/001/Program.cs
+++ Seminar_1/001/Program.cs
@@ -8,8 +8,12 @@ Console.WriteLine( i * i);
 
 
 
+// int i = int.Parse(Console.ReadLine()!);
 
-
+// int i = int.Parse(Console.ReadLine()??"0");
 
                // int i = Int32.Parse(Console.ReadLine());   разобрать
+
+
+               
                
