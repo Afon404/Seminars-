b07d6f4102f258038f26168c674fffe8e791857f
Afon404 <>
2023-01-28 14:34:11 +0300

    //    Задать массив, заполнить случайными положительными трёхзначными числами.     Показать количество нечетных\четных чисел

--- Seminar_4/003/Program.cs
+++ Seminar_4/003/Program.cs
@@ -1,3 +1,42 @@
 ﻿
     //    Задать массив, заполнить случайными положительными трёхзначными числами. 
    //     Показать количество нечетных\четных чисел
+
+/////////////////////////////////////////////////////////////////
+
+
+Console.Clear();
+Console.WriteLine();
+
+
+int size = 20;
+
+int[] Array(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+void PrintResult(int[] array)
+{
+    int even = 0;
+    int odd = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+        if (array[i] % 2 == 0) even++;
+        else odd++;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Количество четных чисел {even}, количество нечетных чисел {odd}");
+    Console.WriteLine();
+}
+
+PrintResult(Array(size));
+
+///////////////////////////////////////////////////////////////////////////////
+
