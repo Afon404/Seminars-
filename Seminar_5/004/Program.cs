9265329671bc0ac057248e3da0a49d1373d1c889
Afon404 <>
2023-01-28 20:22:13 +0300

    Seminars_5

---
+++ Seminar_5/004/Program.cs
@@ -0,0 +1,14 @@
+﻿     //Написать программу копирования массива
+
+
+
+
+ 
+
+        int[] arr = { 5, 4, 7, 2, 9 };
+ 
+        int[] copy = new int[arr.Length];
+        arr.CopyTo(copy, 0);
+ 
+        Console.WriteLine(String.Join(", ", copy));        // 5, 4, 7, 2, 9
+ 
