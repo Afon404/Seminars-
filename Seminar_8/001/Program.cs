567ac78d093aa83b42a1448d88c8b69774495d4e
Afon404 <>
2023-02-08 15:34:44 +0300

    Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.

--- Seminar_8/001/Program.cs
+++ Seminar_8/001/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Частотный словарь содержит информацию о том, 
 // сколько раз встречается элемент входных данных.
 
-// Пример: Есть набор данных
+// Пример: Есть набор данных:
 
 // { 1, 9, 9, 0, 2, 8, 0, 9 }
 
@@ -17,33 +17,34 @@
 ///////////////////////////////////////////////////
 
 
-int[] num = new int[10]{0,1,2,3,4,5,6,7,8,9};
-
-int[] count = new int[10];
+int[] num = new int[8]{1, 9, 9, 0, 2, 8, 0, 9 };
+Console.Write("Есть набор данных: " + String.Join(" ", num)+".");
+Console.WriteLine();
+Console.Write("Array of random numbers");
+Console.WriteLine();
+int[] count = new int[8];
 
 int[] array = new int[20];
-
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(0, 10);
+   
+    Console.Write($" { array[i]}, ");
 }
 
-for (int i = 0; i < array.Length; i++)
-{
-    System.Console.Write($" {array[i]} ");
-}
 for (int i = 0; i < num.Length; i++)
 {
     for (int j = 0; j < array.Length; j++)
     {
             if (array[j] == num[i])
                  count[i]++;
-  
     }
-
 }
-System.Console.WriteLine();
+    Console.WriteLine();
 for (int i = 0; i < num.Length; i++)
 {
-    System.Console.WriteLine($" Число {num[i]} было в массиве столько раз {count[i]}");
+    Console.WriteLine($" Число {num[i]} было в массиве {count[i]} раз ");
 }
+
+
+/////////////////////////////////////////////////////
