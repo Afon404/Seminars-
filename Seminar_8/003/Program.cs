48d728235a8fdf67e363630861ae5b2b973602b9
Afon404 <>
2023-02-09 20:18:35 +0300

    Пример частотного массива для текстовых данных String: Входные данные

--- Seminar_8/003/Program.cs
+++ Seminar_8/003/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Пример частотного массива для текстовых данных String: Входные данные:
 
-//основывающийся на предположении о существовании нетривиального статистического распределения отдельных символов и их последовательностей как в открытом тексте, так и шифрованном тексте, которое с точностью до замены символов будет сохраняться в процессе шифрования и дешифрования.
+string text = "Частотный анализ – это один из методов криптоанализа, основывающийся на предположении о существовании нетривиального статистического распределения отдельных символов и их последовательностей как в открытом тексте, так и шифрованном тексте, которое с точностью до замены символов будет сохраняться в процессе шифрования и дешифрования.";
 
 // Частотный анализ может выглядеть так
 
@@ -9,45 +9,84 @@
 // Символ и встречается 26 раз.  Частота 7.78%
 // Символ т встречается 25 раз.  Частота 7.49%
 
-///////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////
 
 
-Console.WriteLine("Введите текст: ");
- string text = Console.ReadLine()!;
 
-char[] chars = new char[36]{'й','ц','у','к','е','н','г','ш','щ',
+string FillArray(string text)
+{
+    char[] chars = new char[34]{'й','ц','у','к','е','н','г','ш','щ',
                             'з','ф','ы','в','а','п','р','о','л',
                             'д','ж','э','я','ч','с','м','и','т',
-                            'ь','б','ю','-','.',',','!','ъ',' ',};
+                            'ь','б','ю','-','.',',',' ',};
+    string fillText = " ";
 
-int[] count = new int[36];
-double res = 0;
+    for (int i = 0; i < chars.Length; i++)
+    {
+        for (int j = 0; j < text.Length; j++)
+        {
+            if (text[j] == chars[i])
+            {
+                fillText += text[j];
+            }
+        }
+    }
 
-for (int i = 0; i < chars.Length; i++)
-{
-    for (int j = 0; j < text.Length; j++)
-    {
-        if (text[j] == chars[i])
-            count[i]++;
-    }
-}
- 
-for (int i = 0; i < count.Length; i++)
-{
-    // Console.WriteLine($"Символ {chars[i]} встречаеться {count[i]} раз. Частота 0 %");
-    for (int j = 0; j < text.Length; j++)
-    {
-           res = count[i]   * 100 /1000;
-        
-       
-    }
-    
-    Console.WriteLine($"Символ {chars[i]} встречаеться {count[i]} раз. Частота {res}%");
+    return fillText;
 }
 
+int[] SearchRepeats(string fillText)
+{
+    int[] repeatsArray = new int[fillText.Length];
+
+    for (int i = 0; i < fillText.Length; i++)
+    {
+        int count = 0;
+
+        for (int j = 0; j < fillText.Length; j++)
+        {
+            if (fillText[i] == fillText[j])
+            {
+                count++;
+            }
+        }
+
+        repeatsArray[i] = count;
+    }
+
+    return repeatsArray;
+}
+
+void PrintRepeats(int[] repeatsArray, string fillText)
+{
+    double frequency = Math.Round(((double)repeatsArray[0] / repeatsArray.Length) * 100, 2);
+    Console.WriteLine($"Символ | пробел |, встречается:  {repeatsArray[0]} раз. Частота: {frequency}%");
+
+    for (int i = 1; i < fillText.Length; i++)
+    {
+        if (fillText[i] != fillText[i - 1])
+        {
+            frequency = Math.Round(((double)repeatsArray[i] / repeatsArray.Length) * 100, 2);
+            Console.WriteLine($"Символ | {fillText[i].ToString()} |встречается {repeatsArray[i]} раз. Частота {frequency}%");
+        }
+    }
+}
+Console.WriteLine(text);
+Console.WriteLine();
+
+string fillText = FillArray(text);
+int[] repeatsArray = SearchRepeats(fillText);
+PrintRepeats(repeatsArray, fillText);
 
 
-////////////////////////////////
+
+///////////////////////////////////////////////
+
+
+
+
+
 // 0-20 = 5%
 
 // 21-40 = 7%
@@ -57,3 +96,6 @@ for (int i = 0; i < count.Length; i++)
 // 61-80 = 9%
 
 // 81-100 = 6%
+
+
+///////////////////////////////////////////////////
