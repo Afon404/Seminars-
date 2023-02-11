2610ab3eb7aca5f962bfe086cd51b1c2e2e3d800
Afon404 <>
2023-02-11 19:22:58 +0300

    Рекурсия:  Написать программу вычисления функции Аккермана:

--- Seminar_9/004/Program.cs
+++ Seminar_9/004/Program.cs
@@ -1,13 +1,70 @@
 ﻿
 
                 //      Рекурсия:
-       //  Написать программу вычисления функции Аккермана
+       //  Написать программу вычисления функции Аккермана:
 
-int Akk(int n,int m)
-{ 
-    if (n == 0) return m + 1;
-    if (n != 0 && m == 0) return Akk(n - 1, 1);
-    if (n > 0 && m > 0) return Akk(n - 1, Akk(n, m - 1));
-    return Akk(n,m);
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
+
+// вызов функции Аккермана
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
 }
-Console.WriteLine(Akk(2,1));
+
+// функция Аккермана
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+
+
+/////////////////////////////////////////////////////////////////
+
+// int Akkerman(int n, int m)
+// {
+//     if (n == 0) return m + 1;
+//     else if (n != 0 && m == 0) return Akkerman(n - 1, 1);
+//     else return Akkerman(n - 1, Akkerman(n, m - 1));
+// }
+
+// int n = 10;
+// int m = 3;
+
+// Console.WriteLine($"Функция Аккермана A({n},{m}):");
+// if (n >= 0 && m >= 0)
+// {
+//     for (int i = 0; i <= n; i++)
+//     {
+//         for (int j = 0; j <= m; j++)
+//         {
+//             Console.Write($"{Akkerman(j, i)} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// else Console.WriteLine("Числа должны быть неотрицательные!");
+
+
+////////////////////////////////////////////////////////////
+
+
