f6ff093fcbd19a7566b7878d44f60e0f7f393603
Afon404 <>
2023-02-11 12:44:36 +0300

    Seminar_9

---
+++ Seminar_9/001/Program.cs
@@ -0,0 +1,29 @@
+﻿            
+            // Рекурсия:
+  //  Показать натуральные числа от N до 1, N задано:
+
+System.Console.Write("Введиче число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+void PrintArray(int n)
+{
+   if (n >= 0 )
+    {
+        Console.Write(n + " ");   //  2.
+        PrintArray(n - 1);         // 1.   ( Поменять местами 1 и 2 в обратном порядке
+    }
+}
+    
+PrintArray(n);
+
+
+/////////////// Не мой:
+
+// Показать натуральные числа от N до 1, N задано
+
+string NumbersRec(int n)
+{
+    if(n <= 9) return NumbersRec(n + 1) + $"{n}";
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec(1));
---
+++ Seminar_9/002/Program.cs
@@ -0,0 +1,60 @@
+﻿
+                      //// Рекурсия:
+   //  Показать натуральные числа от M до N, N и M заданы:
+
+///////////////////////  Вариант 1:
+
+// int n = InputNumbers("Введите число M: ");
+// int m = InputNumbers("Введите число N: ");
+
+// void NaturalNumbers(int m, int n)
+// {
+//     if (n < m) Console.Write($"");
+//     else
+//     {
+//         NaturalNumbers(m, n - 1);
+//         Console.Write($"{n} ");
+//     }
+// }
+
+// if (m > 0 && n > 0)
+// {
+//     if (m > n)
+//     {
+//         int temp = m;
+//         m = n;
+//         n = temp;
+//     }
+//     System.Console.WriteLine($"Натуральные числа от {m} до {n}:");
+//     NaturalNumbers(m, n);
+// }
+// else Console.WriteLine("Ошибка. Натуральным является число больше нуля!");
+
+// /////////////////////////////
+
+// int InputNumbers(string input)
+// {
+//   Console.Write(input);
+//   int output = Convert.ToInt32(Console.ReadLine());
+//   return output;
+// }
+
+
+
+
+/////////////// Вариант 2:  ///////////// Чила введены: от M до N, а показанны: от N до M.
+
+int n = InputNumbers("Введите число M: ");
+int m = InputNumbers("Введите число N: ");
+string NumbersRec(int n,int  m)
+{
+    if(n <= m) return NumbersRec(n + 1, m) + $"{n}";
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec(n,m));
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
